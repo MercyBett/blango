29f0b82aff4c3f1aed4038c8e6cba142822e536d
Mercybett <merjep551@gmail.com>
2023-02-15 05:28:04 +0300

    create tag and post models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Tag(models.Model):
+  value = models.CharField(max_length=50)
+
+  def __str__(self):
+    return self.value
+
+class Post(models.Model):
+  title = models.CharField(max_length=125)
+  content = models.TextField()
+  summary = models.TextField(max_length=255)
+  slug = models.SlugField(max_length=50)
+  tags = models.ManyToManyField(Tag, blank=True)
+  create_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now_add=True)
+  published = models.DateTimeField(auto_now_add=False)
